4529ca8475a225804334f79c4389ff4ffe3e2c67
romanperno <romanperno@gmail.com>
2024-10-30 17:02:36 -0400

    audio

--- Assets/Scripts/ZoneChaudron.cs
+++ Assets/Scripts/ZoneChaudron.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ZoneChaudron : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioSource1;
+    [SerializeField] private AudioSource audioSource2;
+
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entering object has a Rigidbody (meaning it's a droppable object)
         if (other.gameObject.GetComponent<Rigidbody>() != null)
         {
             ScoreManager.Instance.AddPoint();
+            audioSource1.Play();
+            audioSource2.Play();
             Destroy(other.gameObject);
         }
     }
